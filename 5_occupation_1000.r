497e86cfbc50495fee520d79e849617cfdc95a36
tt22567 <115807819+tt22567@users.noreply.github.com>
2024-10-14 18:29:01 +0100

    Add files via upload

---
+++ 5_occupation_1000.r
@@ -0,0 +1,338 @@
+#Xiumei Lu code
+
+#find the center 1000 genes
+#plot bins of each property
+
+#-------------------# 
+# CLEAN ENVIRONMENT #
+#-------------------#
+rm( list = ls() )
+
+#---------------#
+# LOAD PACKAGES #
+#---------------#
+library(ape)
+library(ggplot2)
+library(cowplot)  #plot_grid
+library(viridis)
+
+#-----------------------#
+# SET WORKING DIRECTORY #
+#-----------------------#
+
+# Load package for automatically finding the path to this R script
+library( rstudioapi ) 
+
+# Get the path to current open R script
+path_to_file <- rstudioapi::getActiveDocumentContext()$path
+full_path_to_file <- base::dirname( path_to_file )
+
+# find and replace characters in your path
+wd <- gsub( pattern = "Scripts",
+            replacement = "csv",
+            x = full_path_to_file )
+
+#fig directory
+fig_dir <- gsub( pattern = "Scripts",
+                 replacement = "Figs/occupation/",
+                 x = full_path_to_file )
+
+#csv directory
+csv_dir <- gsub( pattern = "Scripts",
+                 replacement = "csv/criteria_1000/",
+                 x = full_path_to_file )
+
+#Rdata directory
+Rdata_dir <- gsub( pattern = "Scripts",
+                   replacement = "Rdata/",
+                   x = full_path_to_file )
+
+setwd( wd )
+
+#-------#
+# TASKS #
+#-------#
+
+#---------------------------------------------------#
+#    choose central 1000 trees in multidimension    #
+#             use distance matrix                   #
+#---------------------------------------------------#
+
+#read the distance matrix
+input <- paste0(wd, "/res_tree_distance_BMGE_Normalise_noNA_2041.csv")
+dist_mat <- read.csv( file = input, row.names = 1)
+
+# Calculate the sum of each row
+row_sums <- rowSums(dist_mat)
+
+# Sort the row sums in descending order
+sorted_row_sums <- sort(row_sums, decreasing = FALSE)
+
+# Extract the top 1000 rows
+top_1000_row_sums <- sorted_row_sums[1:1000]
+
+# Print the row names of the top 1000 rows
+name_1000 <- names(top_1000_row_sums)
+print(name_1000)
+
+# Save the names of the 1000 closest points 
+output <- paste0(wd,"/closest1000.csv")
+#write(name_1000, file = output)  
+
+#-------------------------------------------#
+# plot closest 1000 genes on the tree space #
+#-------------------------------------------#
+
+#load distance matrix
+input <- paste0(Rdata_dir,'1_TreeDistance.Rdata')
+load(input)
+
+centre_1000 <- name_1000
+
+center_indices <- which(rownames(pca_res$x) %in% centre_1000)
+
+# Create a dataframe for the highlighted points
+highlighted_df <- data.frame(
+  PC1 = pca_res$x[center_indices, 1],
+  PC2 = pca_res$x[center_indices, 2],
+  label = "Center_1000"
+)
+
+plot <- ggplot(data = pca_res$x, aes(PC1, PC2)) +
+  geom_point(size = 0.5, alpha = 1.0) +
+  geom_point(data = highlighted_df, aes(PC1, PC2, color = label), size = 0.5, show.legend = TRUE) +
+  # Manually specify the colors and labels for the legend in a plot
+  scale_color_manual(values = "#AD0B08", labels = c("Center_1000")) +
+  labs(color = "") +
+  theme_gray() +  # default grey theme
+  #theme_classic() +  # classic empty theme
+  theme(
+    legend.position = "top",
+    plot.title = element_text(size = 10),
+    axis.text = element_text(size = 10),  # Preserve axis tick labels for small plots
+    legend.text = element_text(size = 10),  # Adjust the font size of the legend text
+    axis.title = element_text(size = 10)   # Set axis title size
+  ) +
+  xlab("PC1 (73.7%)") +
+  ylab("PC2 (3.2%)")   # Set axis labels
+
+# Save
+output <- paste0(fig_dir, "closest1000.tiff")
+ggsave(plot, filename = output, width = 6, height = 3, units = "in", dpi = 300)
+
+
+#------------------------------------------------#
+# how different properties capture central genes #
+#------------------------------------------------#
+
+centre_1000 <- name_1000
+
+center_indices <- which(rownames(pca_res$x) %in% centre_1000)
+
+# Read the CSV file containing the gene names to be mapped
+properties <- read.csv("properties_final_2041.csv", header = TRUE)
+
+# Columns to be sorted in descending order
+prop_positive_desc <- c("NS","AL","PPIS","NVS","LHF","ABSS","TTL","Treeness","TreenessdeRCV","PC2","ER")
+
+# Columns to be sorted in ascending order
+prop_negative_asc <- c("PG","RCV","LHU","ER","ALBS","Saturation") 
+  
+# Create an empty list to store the plots
+plot_list <- list()
+
+i <- 1
+
+# Loop through each group of genes (sorted in descending order)
+for (column in prop_positive_desc) {
+  
+  # Sort the dataframe by the current column in descending order
+  sorted_properties <- properties[order(-properties[[column]]), ]
+  
+  # Select the top 1000 genes
+  top_1000_genes <- sorted_properties[1:1000, ]
+  
+  # Extract the "tree" column for the top 1000 genes
+  top_1000_trees <- top_1000_genes$rownames.tree
+  
+  #save
+  output <- paste0(csv_dir,column,"_MAX_1000.csv")
+  #write.csv(top_1000_trees, file = output)
+  
+  # Find the positions of the current group of genes in the PCA results
+  top1000_indices <- which(rownames(pca_res$x) %in% top_1000_trees)
+
+  # Find the shared genes between the centre 1000 genes and the top 1000 genes for the current column
+  shared_trees <- centre_1000[centre_1000 %in% top_1000_trees]
+  
+  # Find the positions of the shared genes in the PCA results
+  sharetree_indices <- which(rownames(pca_res$x) %in% shared_trees)
+  
+  # Calculate the number of shared genes
+  occup <- length(shared_trees)
+  
+  # Property name for the legend
+  file_id <- paste(column, "MAX_1000", sep = "_")
+  
+  # Define the color and label vectors
+  labels <- c("Center_1000", file_id, paste('Shared', occup, sep = "_" ))
+  colors <- c("gold", "blue", "#FF0000")  # Colors for the plot
+  
+  # Create the plot and save it
+  current_plot <- ggplot(data = pca_res$x, aes(PC1, PC2)) +
+    geom_point(color = 'black', size = 1.0, alpha = 1.0) +
+    geom_point(data = pca_res$x[center_indices,], aes(PC1, PC2, color = labels[1]), size = 1.0, show.legend = TRUE) +
+    geom_point(data = pca_res$x[top1000_indices,], aes(PC1, PC2, color = labels[2]), size = 1.0, show.legend = TRUE) +
+    geom_point(data = pca_res$x[sharetree_indices,], aes(PC1, PC2, color = labels[3]), size = 1.0, show.legend = TRUE) +
+    scale_color_manual(labels = labels, values = colors) +
+    labs(color = " ") +  # Set the title of the legend
+    theme(legend.position = "top",
+          #panel.background = element_rect(fill = "grey"),
+          #panel.grid.major = element_blank(),
+          #panel.grid.minor = element_blank(),
+          plot.title = element_text(size = 18),
+          axis.text = element_text(size = 18),  # Preserve the axis tick labels for small plots
+          legend.text = element_text(size = 18),  # Adjust the font size of the legend text
+          axis.title = element_text(size = 18)) + # Set the axis title size
+    xlab("PC1 (73.7%)") +
+    ylab("PC2 (3.2%)")  # Set the axis labels
+  
+  # Add the plot to the list
+  plot_list[[i]] <- current_plot
+  i <- i + 1
+}
+
+# Loop through each group of genes (sorted in ascending order)
+for (column in prop_negative_asc) {
+  
+  # Sort the dataframe by the current column in ascending order and select the top 1000 rows
+  sorted_properties <- properties[order(properties[[column]]), ]
+  
+  # Select the top 1000 genes
+  top_1000_genes <- sorted_properties[1:1000, ]
+  
+  # Extract the "tree" column for the top 1000 genes
+  top_1000_trees <- top_1000_genes$rownames.tree
+  
+  #save
+  output <- paste0(csv_dir,column,"_MIN_1000.csv")
+  #write.csv(top_1000_trees, file = output)
+  
+  # Find the positions of the current group of genes in the PCA results
+  gene_indices_group <- which(rownames(pca_res$x) %in% top_1000_trees)
+  
+  # Find the shared genes between the centre 1000 genes and the top 1000 genes for the current column
+  share_trees <- centre_1000[centre_1000 %in% top_1000_trees]
+  
+  # Find the positions of the shared genes in the PCA results
+  sharetree_indices <- which(rownames(pca_res$x) %in% share_trees)
+  
+  # Calculate the number of shared genes to display in the top right corner
+  occup <- length(share_trees)
+  
+  # Property name for the legend
+  file_id <- paste(column, "MIN_1000", sep = "_")
+  
+  # Define the color and label vectors
+  labels <- c("Center_1000", file_id, paste('Shared', occup, sep = "_"))
+  colors <- c("gold", "blue", "#FF0000")  # Colors for the plot
+  
+  # Create the plot and save it
+  current_plot <- ggplot(data = pca_res$x, aes(PC1, PC2)) +
+    geom_point(color = 'black', size = 1.0, alpha = 1.0) +
+    geom_point(data = pca_res$x[center_indices,], aes(PC1, PC2, color = labels[1]), size = 1.0, show.legend = TRUE) +
+    geom_point(data = pca_res$x[top1000_indices,], aes(PC1, PC2, color = labels[2]), size = 1.0, show.legend = TRUE) +
+    geom_point(data = pca_res$x[sharetree_indices,], aes(PC1, PC2, color = labels[3]), size = 1.0, show.legend = TRUE) +
+    scale_color_manual(labels = labels, values = colors) +
+    labs(color = " ") +
+    theme(legend.position = "top",
+          #panel.background = element_rect(fill = "grey"),
+          #panel.grid.major = element_blank(),
+          #panel.grid.minor = element_blank(),
+          plot.title = element_text(size = 18),
+          axis.text = element_text(size = 18),  # Preserve the axis tick labels for small plots
+          legend.text = element_text(size = 18),  # Adjust the font size of the legend text
+          axis.title = element_text(size = 18)) + # Set the axis title size
+    xlab("PC1 (73.7%)") +
+    ylab("PC2 (3.2%)")  # Set the axis labels
+  
+  # Add the plot to the list
+  plot_list[[i]] <- current_plot
+  i <- i + 1
+}
+
+
+#select the 1000 genes with medium evolutionary rate
+
+# Calculate the median value
+median_ER <- median(properties$ER)
+
+# Compute the absolute differences from the median
+properties$abs_diff_from_median <- abs(properties$ER - median_ER)
+
+# Sort the data frame by the absolute differences
+sorted_properties <- properties[order(properties$abs_diff_from_median), ]
+
+# Select the top 1000 values closest to the median
+values_median_1000 <- sorted_properties[1:1000, ]
+
+# Extract the name for the selected trees
+name_median_1000 <- values_median_1000$rownames.tree
+
+#save
+output <- paste0(csv_dir,"ER_IM_1000.csv")
+#write.csv(name_median_1000, file = output)
+
+# Find the indices of these trees in the PCA results
+indices_median_1000 <- which(rownames(pca_res$x) %in% name_median_1000)
+
+#find the shared trees
+share_trees <- centre_1000[centre_1000 %in% name_median_1000]
+
+# Extract indices of shared genes in the PCA analysis results
+sharetree_indices <- which(rownames(pca_res$x) %in% share_trees)
+
+# Number of shared genes to be displayed in the top right corner
+occup <- length(share_trees)
+
+# Prepare property name for legend
+file_id <- paste( "ER_IM_1000", sep = "_" )
+
+# Define color vector and label vector
+labels <- c("Center_1000",file_id, paste('Shared', occup, sep = "_" ))
+colors <- c("gold", "blue", "#FF0000")   ##E95146
+
+# Construct and save the plot
+current_plot <- ggplot(data = pca_res$x, aes(PC1, PC2)) +
+  geom_point(color = 'black',size = 1.0, alpha = 1.0) +
+  geom_point(data = pca_res$x[center_indices,], aes(PC1, PC2, color = labels[1]), size = 1.0, show.legend = TRUE) +
+  geom_point(data = pca_res$x[indices_median_1000,], aes(PC1, PC2, color = labels[2]), size = 1.0, show.legend = TRUE) +
+  geom_point(data = pca_res$x[sharetree_indices,], aes(PC1, PC2, color = labels[3]), size = 1.0, show.legend = TRUE) +
+  #manually specify the colors and labels for the legend in a plot
+  scale_color_manual(labels = labels,values = colors) +
+  labs(color = " ") +
+  theme(legend.position = "top",
+        #panel.background = element_rect(fill = "grey"),
+        #panel.grid.major = element_blank(),
+        #panel.grid.minor = element_blank(),
+        plot.title = element_text(size = 18),
+        axis.text = element_text(size = 18),  # Preserve the axis tick labels for small plots
+        legend.text = element_text(size = 18),  # Adjust the font size of the legend text
+        axis.title = element_text(size = 18)) + # Set the axis title size
+  xlab("PC1 (73.7%)") +
+  ylab("PC2 (3.2%)")  # Set the axis labels
+
+# Add the current plot to the list
+plot_list[[i]] <- current_plot
+i <- i + 1
+
+# Combine all small plots into one large plot
+combined_plot <- plot_grid(plotlist = plot_list, ncol = 3, align = "h")                                                                
+
+# Save the combined large plot
+output <- paste0(fig_dir,"occupation_1000.tiff")
+ggsave(output, plot = combined_plot, width = 25, height = 23, dpi = 300)
+
+# Save the workspace to a file
+output <- paste0(Rdata_dir, "5_occupation_1000.RData") 
+save.image(file = output)
---
+++ 8_permutation_test_500.r
@@ -0,0 +1,181 @@
+#Xiumei Lu code
+
+#-------------------# 
+# CLEAN ENVIRONMENT #
+#-------------------#
+rm( list = ls() )
+
+#---------------#
+# LOAD PACKAGES #
+#---------------#
+library(phangorn)
+library(Quartet)
+library(future.apply)
+library(ggplot2)
+library(factoextra)
+library(phytools)
+library(ggfortify)
+library(future)
+library(viridis)
+
+#-----------------------#
+# SET WORKING DIRECTORY #
+#-----------------------#
+# Load package for automatically finding the path to this R script
+library( rstudioapi ) 
+# Get the path to current open R script
+path_to_file <- rstudioapi::getActiveDocumentContext()$path
+full_path_to_file <- base::dirname( path_to_file )
+
+# working directory
+wd <- gsub( pattern = "Scripts",
+            replacement = "csv",
+            x = full_path_to_file )
+
+#fig directory
+fig_dir <- gsub( pattern = "Scripts",
+                 replacement = "Figs/",
+                 x = full_path_to_file )
+
+#csv directory
+csv_dir <- gsub( pattern = "Scripts",
+                 replacement = "csv/criteria_1000/",
+                 x = full_path_to_file )
+
+#Rdata directory
+Rdata_dir <- gsub( pattern = "Scripts",
+                   replacement = "Rdata/",
+                   x = full_path_to_file )
+
+# find and replace characters in your path
+setwd( wd )
+
+#-------#
+# TASKS #
+#-------#
+
+#---------------------------------------------------#
+#      Randomly select 1000 genes 100 times         #
+#    calculate the frequency-centre gene plot       #
+#---------------------------------------------------#
+
+# Load necessary libraries
+library(dplyr)
+
+# Load the list of tree files, center genes, number of center genes captured by properties
+alltree2041 <- read.csv('alltree2041.csv', header = TRUE)
+centre_500 <- read.csv('closest500.csv', header = FALSE)
+centre_pros <- read.csv('num_shared_gene_top500.csv', header = TRUE)
+
+head(alltree2041)
+head(centre_500)
+head(centre_pros)
+
+set.seed(42) # For reproducibility
+# Create an empty dataframe to store gene names and their selection counts
+gene_selection_data <- data.frame(gene = character(), selection_count = integer(), stringsAsFactors = FALSE)
+
+# Initialize a vector to store the frequency of selecting centre genes
+selection_frequencies <- integer(100)
+
+# Perform 100 samplings
+for (i in 1:100) {
+  # Randomly select 1000 genes from the 'tree' column
+  sampled_genes <- sample(alltree2041$tree, 500)
+  
+  # Update the selection count for each selected gene
+  for (gene in sampled_genes) {
+    if (gene %in% gene_selection_data$gene) {
+      gene_selection_data$selection_count[gene_selection_data$gene == gene] <- 
+        gene_selection_data$selection_count[gene_selection_data$gene == gene] + 1
+    } else {
+      gene_selection_data <- rbind(gene_selection_data, data.frame(gene = gene, selection_count = 1, stringsAsFactors = FALSE))
+    }
+  }
+  
+  # Calculate how many of these sampled genes are in centre_1000
+  num_centre_genes <- sum(sampled_genes %in% centre_500$V1)
+  
+  # Store the count in the frequencies vector
+  selection_frequencies[i] <- num_centre_genes
+}
+
+# View the results
+head(gene_selection_data)
+
+# Sort the dataframe by selection_count in descending order and select the top 500
+top_500_genes <- gene_selection_data %>%
+  arrange(desc(selection_count)) %>%
+  head(500)
+
+#select the top 500 genes with most frequency
+randomness_MAX_500 <- top_500_genes$gene
+
+#save
+output <- paste0(csv_dir, "randomness_MAX_500.csv")
+#write.csv(randomness_MAX_500, file = output)
+
+# Calculate the frequency distribution
+frequency_distribution <- table(selection_frequencies)
+
+# Print the frequency distribution
+print(frequency_distribution)
+
+# generate colors for legends
+generate_desaturated_colors <- function(n, saturation = 1.0) {
+  hues <- seq(0, 360, length.out = n + 1)[-1]
+  colors <- hcl(h = hues, c = saturation * 100, l = 70)
+  return(colors)
+}
+
+# set the number
+n <- nrow(centre_pros)  # Number of colors needed
+colors <- generate_desaturated_colors(n)
+#Plot to show the generated colors
+#barplot(rep(1, n), col = colors, border = NA, space = 0, main = "Desaturated Colors")
+
+# generate colors for Histogram
+#col <- viridis(n = 10, alpha = 1)
+
+#set the types for legends
+tip.type <- c(1:19,1:2)
+
+# Set the output file path for PDF
+output <- paste0(fig_dir, "Permutation_test_500.pdf")
+
+# Open PDF device with wider width
+pdf(output, width = 14, height = 8.5)  # Increased width to 14 inches, height remains 8.5 inches (standard A4 size)
+
+# Set graphical parameters
+par(#lwd = 3.0,
+    cex.lab = 1.5,   # Axis labels (xlab, ylab)
+    cex.main = 1.5,  # Main title (main)
+    cex.axis = 1.5)  # Axis tick labels
+
+# Create histogram
+r <- hist(selection_frequencies, breaks = 30, 
+          col = "#82AFF9", # #82AFF9
+          #border = "blue",  #pink
+          main = "Frequency of Random Selection of Central Genes",
+          xlab = "Number of Center Genes Captured by Properties", ylab = "Frequency",
+          xlim = c(min(selection_frequencies, centre_pros$central_gene) - 30, 
+                   max(selection_frequencies, centre_pros$central_gene) + 10))
+
+# Add points
+points(centre_pros$central_gene, rep(0, nrow(centre_pros)), col = colors, pch = tip.type, cex = 1.5)
+
+# Add legend
+legend("topright", legend = centre_pros$criteria, col = colors, 
+       pch = tip.type, cex = 1.5, title = " ", box.lty = 0, inset = c(0.1, 0.02))
+
+# Customize axis lines
+axis(side = 1, lwd = 2)  # X axis
+axis(side = 2, lwd = 2)  # Y axis
+
+# Close the PDF device
+dev.off()
+
+# Save the workspace to a file
+output <- paste0(Rdata_dir,"Permutation_test.RData")
+#save.image(file = output)
+
