bede2f9e3defd6dd37f72cb580c3b4c0c8cbabdf
XiumeiLu <115807819+tt22567@users.noreply.github.com>
2025-03-06 22:38:27 +0000

    Update 6_occupation_500_nmds.r

--- 6_occupation_500.r
+++ 6_occupation_500.r
@@ -54,12 +54,12 @@ Rdata_dir <- gsub( pattern = "Scripts",
 #-------#
 
 #---------------------------------------------------#
-#    choose central 500 trees in multidimension    #
+#           choose central 500 trees                #
 #             use distance matrix                   #
 #---------------------------------------------------#
 
 #read the distance matrix
-input <- paste0(wd,"/res_tree_distance_BMGE_Normalise_noNA_2041.csv")
+input <- paste0(wd, "/res_tree_distance_BMGE_Normalise_noNA_2041.csv")
 dist_mat <- read.csv( file = input, row.names = 1)
 
 # Calculate the sum of each row
@@ -76,57 +76,61 @@ name_500 <- names(top_500_row_sums)
 print(name_500)
 
 # Save the names of the 500 closest points 
-output <- paste0(wd, "/closest500.csv")
-write(name_500, file = output)  
+output <- paste0(wd,"/closest500.csv")
+#write(name_500, file = output)  
 
 #-------------------------------------------#
 # plot closest 500 genes on the tree space #
 #-------------------------------------------#
 
 #load distance matrix
-input <- paste0(Rdata_dir,'1_TreeDistance.Rdata')
+input <- paste0(Rdata_dir,'1_TreeDistance_NMDS.Rdata')
 load(input)
+
+#load nmds analysis result
+nmds2
 
 centre_500 <- name_500
 
-center_indices <- which(rownames(pca_res$x) %in% centre_500)
+center_indices <- which(rownames(nmds2$points) %in% centre_500)
 
 # Create a dataframe for the highlighted points
 highlighted_df <- data.frame(
-  PC1 = pca_res$x[center_indices, 1],
-  PC2 = pca_res$x[center_indices, 2],
-  label = "closest_500"
+  MDS1 = nmds2$points[center_indices, 1],
+  MDS2 = nmds2$points[center_indices, 2],
+  label = "Center_500"
 )
 
-plot <- ggplot(data = pca_res$x, aes(PC1, PC2)) +
+plot <- ggplot(data = nmds2$points, aes(MDS1, MDS2)) +
   geom_point(size = 0.5, alpha = 1.0) +
-  geom_point(data = highlighted_df, aes(PC1, PC2, color = label), size = 0.5, show.legend = TRUE) +
+  geom_point(data = highlighted_df, aes(MDS1, MDS2, color = label), size = 0.5, show.legend = TRUE) +
   # Manually specify the colors and labels for the legend in a plot
-  scale_color_manual(values = "red", labels = c("closest_500")) +
+  scale_color_manual(values = "#AD0B08", labels = c("Center_500")) +
   labs(color = "") +
   theme_gray() +  # default grey theme
   #theme_classic() +  # classic empty theme
   theme(
     legend.position = "top",
-    plot.title = element_text(size = 6),
-    axis.text = element_text(size = 6),  # Preserve axis tick labels for small plots
-    legend.text = element_text(size = 6),  # Adjust the font size of the legend text
-    axis.title = element_text(size = 6)   # Set axis title size
+    plot.title = element_text(size = 10),
+    axis.text = element_text(size = 10),  # Preserve axis tick labels for small plots
+    legend.text = element_text(size = 10),  # Adjust the font size of the legend text
+    axis.title = element_text(size = 10)   # Set axis title size
   ) +
-  xlab("PC1 (73.7%)") +
-  ylab("PC2 (3.2%)")   # Set axis labels
+  xlab("NMDS1") +
+  ylab("NMDS2")   # Set axis labels
 
 # Save
-output <- paste0(fig_dir, "closest500.tiff")
-#ggsave(plot, filename = output, width = 6, height = 3, units = "in", dpi = 300)
+output <- paste0(fig_dir, "closest500_NMDS.tiff")
+ggsave(plot, filename = output, width = 6.5, height = 5, units = "in", dpi = 300)
 
-#-------------------------------------------------#
-# how different properties capture central genes  #
-#-------------------------------------------------#
 
-Center_500 <- name_500
+#------------------------------------------------#
+# how different properties capture central genes #
+#------------------------------------------------#
 
-center_indices <- which(rownames(pca_res$x) %in% Center_500)
+centre_500 <- name_500
+
+center_indices <- which(rownames(nmds2$points) %in% centre_500)
 
 # Read the CSV file containing the gene names to be mapped
 properties <- read.csv("properties_final_2041.csv", header = TRUE)
@@ -155,17 +159,17 @@ for (column in prop_positive_desc) {
   top_500_trees <- top_500_genes$rownames.tree
   
   #save
-  #output <- paste0(csv_dir,column,"_MAX_500.csv")
+  output <- paste0(csv_dir,column,"_MAX_500.csv")
   #write.csv(top_500_trees, file = output)
   
   # Find the positions of the current group of genes in the PCA results
-  top500_indices <- which(rownames(pca_res$x) %in% top_500_trees)
+  top500_indices <- which(rownames(nmds2$points) %in% top_500_trees)
 
-  # Find the shared genes between the Center 500 genes and the top 500 genes for the current column
-  shared_trees <- Center_500[Center_500 %in% top_500_trees]
+  # Find the shared genes between the centre 500 genes and the top 500 genes for the current column
+  shared_trees <- centre_500[centre_500 %in% top_500_trees]
   
   # Find the positions of the shared genes in the PCA results
-  sharetree_indices <- which(rownames(pca_res$x) %in% shared_trees)
+  sharetree_indices <- which(rownames(nmds2$points) %in% shared_trees)
   
   # Calculate the number of shared genes
   occup <- length(shared_trees)
@@ -175,26 +179,26 @@ for (column in prop_positive_desc) {
   
   # Define the color and label vectors
   labels <- c("Center_500", file_id, paste('Shared', occup, sep = "_" ))
-  colors <- c("gold", "blue", "red")  # Colors for the plot
+  colors <- c("gold", "blue", "#FF0000")  # Colors for the plot
   
   # Create the plot and save it
-  current_plot <- ggplot(data = pca_res$x, aes(PC1, PC2)) +
-    geom_point(color = 'white', size = 1.0, alpha = 1.0) +
-    geom_point(data = pca_res$x[center_indices,], aes(PC1, PC2, color = labels[1]), size = 1.0, show.legend = TRUE) +
-    geom_point(data = pca_res$x[top500_indices,], aes(PC1, PC2, color = labels[2]), size = 1.0, show.legend = TRUE) +
-    geom_point(data = pca_res$x[sharetree_indices,], aes(PC1, PC2, color = labels[3]), size = 1.0, show.legend = TRUE) +
+  current_plot <- ggplot(data = nmds2$points, aes(MDS1, MDS2)) +
+    geom_point(color = 'black', size = 1.5, alpha = 1.0) +
+    geom_point(data = nmds2$points[center_indices,], aes(MDS1, MDS2, color = labels[1]), size = 1.0, show.legend = TRUE) +
+    geom_point(data = nmds2$points[top500_indices,], aes(MDS1, MDS2, color = labels[2]), size = 1.0, show.legend = TRUE) +
+    geom_point(data = nmds2$points[sharetree_indices,], aes(MDS1, MDS2, color = labels[3]), size = 1.0, show.legend = TRUE) +
     scale_color_manual(labels = labels, values = colors) +
     labs(color = " ") +  # Set the title of the legend
     theme(legend.position = "top",
-          panel.background = element_rect(fill = "black"),
+          #panel.background = element_rect(fill = "grey"),
+          #panel.grid.major = element_blank(),
+          #panel.grid.minor = element_blank(),
           plot.title = element_text(size = 18),
           axis.text = element_text(size = 18),  # Preserve the axis tick labels for small plots
           legend.text = element_text(size = 18),  # Adjust the font size of the legend text
-          axis.title = element_text(size = 18),  # Set the axis title size
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank()) +
-    xlab("PC1 (73.7%)") +
-    ylab("PC2 (3.2%)")  # Set the axis labels
+          axis.title = element_text(size = 18)) + # Set the axis title size
+    xlab("NMDS1") +
+    ylab("NMDS2")  # Set the axis labels
   
   # Add the plot to the list
   plot_list[[i]] <- current_plot
@@ -214,17 +218,17 @@ for (column in prop_negative_asc) {
   top_500_trees <- top_500_genes$rownames.tree
   
   #save
-  #output <- paste0(csv_dir,column,"_MIN_500.csv")
+  output <- paste0(csv_dir,column,"_MIN_500.csv")
   #write.csv(top_500_trees, file = output)
   
   # Find the positions of the current group of genes in the PCA results
-  gene_indices_group <- which(rownames(pca_res$x) %in% top_500_trees)
+  gene_indices_group <- which(rownames(nmds2$points) %in% top_500_trees)
   
-  # Find the shared genes between the Center 500 genes and the top 500 genes for the current column
-  share_trees <- Center_500[Center_500 %in% top_500_trees]
+  # Find the shared genes between the centre 500 genes and the top 500 genes for the current column
+  share_trees <- centre_500[centre_500 %in% top_500_trees]
   
   # Find the positions of the shared genes in the PCA results
-  sharetree_indices <- which(rownames(pca_res$x) %in% share_trees)
+  sharetree_indices <- which(rownames(nmds2$points) %in% share_trees)
   
   # Calculate the number of shared genes to display in the top right corner
   occup <- length(share_trees)
@@ -234,26 +238,26 @@ for (column in prop_negative_asc) {
   
   # Define the color and label vectors
   labels <- c("Center_500", file_id, paste('Shared', occup, sep = "_"))
-  colors <- c("gold", "blue", "red")  # Colors for the plot
+  colors <- c("gold", "blue", "#FF0000")  # Colors for the plot
   
   # Create the plot and save it
-  current_plot <- ggplot(data = pca_res$x, aes(PC1, PC2)) +
-    geom_point(color = 'grey', size = 1.0, alpha = 1.0) +
-    geom_point(data = pca_res$x[center_indices,], aes(PC1, PC2, color = labels[1]), size = 1.0, show.legend = TRUE) +
-    geom_point(data = pca_res$x[top500_indices,], aes(PC1, PC2, color = labels[2]), size = 1.0, show.legend = TRUE) +
-    geom_point(data = pca_res$x[sharetree_indices,], aes(PC1, PC2, color = labels[3]), size = 1.0, show.legend = TRUE) +
+  current_plot <- ggplot(data = nmds2$points, aes(MDS1, MDS2)) +
+    geom_point(color = 'black', size = 1.5, alpha = 1.0) +
+    geom_point(data = nmds2$points[center_indices,], aes(MDS1, MDS2, color = labels[1]), size = 1.0, show.legend = TRUE) +
+    geom_point(data = nmds2$points[top500_indices,], aes(MDS1, MDS2, color = labels[2]), size = 1.0, show.legend = TRUE) +
+    geom_point(data = nmds2$points[sharetree_indices,], aes(MDS1, MDS2, color = labels[3]), size = 1.0, show.legend = TRUE) +
     scale_color_manual(labels = labels, values = colors) +
     labs(color = " ") +
     theme(legend.position = "top",
-          panel.background = element_rect(fill = "black"),
+          #panel.background = element_rect(fill = "grey"),
+          #panel.grid.major = element_blank(),
+          #panel.grid.minor = element_blank(),
           plot.title = element_text(size = 18),
           axis.text = element_text(size = 18),  # Preserve the axis tick labels for small plots
           legend.text = element_text(size = 18),  # Adjust the font size of the legend text
-          axis.title = element_text(size = 18),  # Set the axis title size
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank()) +
-    xlab("PC1 (73.7%)") +
-    ylab("PC2 (3.2%)")  # Set the axis labels
+          axis.title = element_text(size = 18)) + # Set the axis title size
+    xlab("NMDS1") +
+    ylab("NMDS2")  # Set the axis labels
   
   # Add the plot to the list
   plot_list[[i]] <- current_plot
@@ -279,17 +283,17 @@ values_median_500 <- sorted_properties[1:500, ]
 name_median_500 <- values_median_500$rownames.tree
 
 #save
-#output <- paste0(csv_dir,"ER_IM_500.csv")
+output <- paste0(csv_dir,"ER_IM_500.csv")
 #write.csv(name_median_500, file = output)
 
 # Find the indices of these trees in the PCA results
-indices_median_500 <- which(rownames(pca_res$x) %in% name_median_500)
+indices_median_500 <- which(rownames(nmds2$points) %in% name_median_500)
 
 #find the shared trees
-share_trees <- Center_500[Center_500 %in% name_median_500]
+share_trees <- centre_500[centre_500 %in% name_median_500]
 
 # Extract indices of shared genes in the PCA analysis results
-sharetree_indices <- which(rownames(pca_res$x) %in% share_trees)
+sharetree_indices <- which(rownames(nmds2$points) %in% share_trees)
 
 # Number of shared genes to be displayed in the top right corner
 occup <- length(share_trees)
@@ -299,27 +303,27 @@ file_id <- paste( "ER_IM_500", sep = "_" )
 
 # Define color vector and label vector
 labels <- c("Center_500",file_id, paste('Shared', occup, sep = "_" ))
-colors <- c("gold", "blue", "red")   ##E95146
+colors <- c("gold", "blue", "#FF0000")   ##E95146
 
 # Construct and save the plot
-current_plot <- ggplot(data = pca_res$x, aes(PC1, PC2)) +
-  geom_point(color = 'grey',size = 1.0, alpha = 1.0) +
-  geom_point(data = pca_res$x[center_indices,], aes(PC1, PC2, color = labels[1]), size = 1.0, show.legend = TRUE) +
-  geom_point(data = pca_res$x[indices_median_500,], aes(PC1, PC2, color = labels[2]), size = 1.0, show.legend = TRUE) +
-  geom_point(data = pca_res$x[sharetree_indices,], aes(PC1, PC2, color = labels[3]), size = 1.0, show.legend = TRUE) +
+current_plot <- ggplot(data = nmds2$points, aes(MDS1, MDS2)) +
+  geom_point(color = 'black',size = 1.5, alpha = 1.0) +
+  geom_point(data = nmds2$points[center_indices,], aes(MDS1, MDS2, color = labels[1]), size = 1.0, show.legend = TRUE) +
+  geom_point(data = nmds2$points[indices_median_500,], aes(MDS1, MDS2, color = labels[2]), size = 1.0, show.legend = TRUE) +
+  geom_point(data = nmds2$points[sharetree_indices,], aes(MDS1, MDS2, color = labels[3]), size = 1.0, show.legend = TRUE) +
   #manually specify the colors and labels for the legend in a plot
   scale_color_manual(labels = labels,values = colors) +
   labs(color = " ") +
   theme(legend.position = "top",
-        panel.background = element_rect(fill = "black"),
+        #panel.background = element_rect(fill = "grey"),
+        #panel.grid.major = element_blank(),
+        #panel.grid.minor = element_blank(),
         plot.title = element_text(size = 18),
-        axis.text = element_text(size = 18),  # Preserve axis tick labels for small plots
-        legend.text = element_text(size = 18),  # Adjust the font size of legend text
-        axis.title = element_text(size = 18),   # Set axis title size
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank()) +
-  xlab("PC1 (73.7%)") +
-  ylab("PC2 (3.2%)")  # Set the axis labels
+        axis.text = element_text(size = 18),  # Preserve the axis tick labels for small plots
+        legend.text = element_text(size = 18),  # Adjust the font size of the legend text
+        axis.title = element_text(size = 18)) + # Set the axis title size
+  xlab("NMDS1") +
+  ylab("NMDS2")  # Set the axis labels
 
 # Add the current plot to the list
 plot_list[[i]] <- current_plot
@@ -329,9 +333,9 @@ i <- i + 1
 combined_plot <- plot_grid(plotlist = plot_list, ncol = 3, align = "h")                                                                
 
 # Save the combined large plot
-output <- paste0(fig_dir,"occupation_500.tiff")
-ggsave(plot = combined_plot, output, width = 25, height = 23, dpi = 300)
+output <- paste0(fig_dir,"occupation_500_NMDS.tiff")
+ggsave(output, plot = combined_plot, width = 25, height = 38, dpi = 300)
 
 # Save the workspace to a file
-output <- paste0(Rdata_dir, "5_occupation_500.RData") 
+output <- paste0(Rdata_dir, "6_occupation_500_NMDS.RData") 
 save.image(file = output)
